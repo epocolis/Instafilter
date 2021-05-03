b596e81632839c0e501c48f6cd0e6dc548edf642
leotis <mekdemcrossit@tutanota.com>
2021-05-03 05:33:13 -0400

    Creating a custom binding
    This will not print a the new value of blurAmount, because neither blurAmoun or the Struct struct wrapping it changes

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -9,14 +9,39 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var blurAmount:CGFloat = 0
+    
+    /**
+     This will not print a the new value of blurAmount, because neither blurAmoun or the Struct struct wrapping it changes
+     whenever the users move the slider. The binding is directly changing the internally stored value. This means the property
+     observer is never being triggered.
+     
+     This can be   solved by creating a custom binding
+     
+     This also allow us to  do whatever you want inside these closures: you can call methods, run an algorithm to figure out the
+     correct value to use, or even just use random values – it doesn’t matter, as long as you return a value from get. So, if you
+     want to make sure you update UserDefaults every time a value is changed, the set closure of a Binding is perfect.
+     
+     
+     */
+    @State private var blurAmount: CGFloat = 0
     
     var body: some View {
+        
+        let blur = Binding<CGFloat>(
+            get: {
+                self.blurAmount
+            },
+            set: {
+                self.blurAmount = $0
+                print("New value is \(self.blurAmount)")
+            }
+        )
+        
         VStack {
             Text("Hello, World")
                 .blur(radius: blurAmount)
             
-            Slider(value: $blurAmount, in: 0...20)
+            Slider(value: blur, in: 0...20)
         }
     }
 }
